f857d42c674a72ffa536460ef9f06cc5cf6e587e
Levente Csoke <leventec3@gmail.com>
2020-11-30 15:45:49 +0100

    part 2

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,8 +6,12 @@ import {
   Card,
   CardMedia,
   CardContent,
+  CircularProgress,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import mockData from "../mockData";
+import { toFirstCharUppercase } from "../constants";
 
 const useStyles = makeStyles({
   pokedexContainer: {
@@ -15,32 +19,51 @@ const useStyles = makeStyles({
     paddingLeft: "50px",
     paddingRight: "50px",
   },
+  cardMedia: {
+    margin: "auto",
+    width: "130px",
+    height: "130px",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
 });
 
-const getPokemonCard = () => {
-  return (
-    <Grid item xs={12} sm={4}>
-      <Card>
-        <CardContent>hi</CardContent>
-      </Card>
-    </Grid>
-  );
-};
-
-const Pokedex = () => {
+const Pokedex = (props) => {
+  const { history } = props;
   const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState(mockData);
+
+  const getPokemonCard = (pokemonId) => {
+    console.log(pokemonData[`${pokemonId}`]);
+    const { id, name } = pokemonData[`${pokemonId}`];
+    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    return (
+      <Grid item xs={12} sm={4} key={pokemonId}>
+        <Card onClick={() => history.push(`/${pokemonId}`)}>
+          <CardMedia className={classes.cardMedia} image={sprite} />
+          <CardContent className={classes.cardContent}>
+            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
+
   return (
     <>
       <AppBar position="static">
         <Toolbar />
       </AppBar>
-      <Grid container spacing={2} className={classes.pokedexContainer}>
-        {getPokemonCard()}
-        {getPokemonCard()}
-        {getPokemonCard()}
-        {getPokemonCard()}
-        {getPokemonCard()}
-      </Grid>
+      {pokemonData ? (
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map((pokemonId) =>
+            getPokemonCard(pokemonId)
+          )}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
     </>
   );
 };
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import mockData from "../mockData";
+import { Typography, Link } from "@material-ui/core";
+import { toFirstCharUppercase } from "../constants";
 
 const Pokemon = (props) => {
   const { match } = props;
   const { params } = match;
   const { pokemonId } = params;
+  const [pokemon, setPokemon] = useState(mockData[`${pokemonId}`]);
 
-  return <div>{`This is the pokemon page for #${pokemonId}`}</div>;
+  const generatePokemonJSX = () => {
+    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+    const { front_default } = sprites;
+
+    return (
+      <>
+        <Typography variant="h2">
+          {`${id}.`} {toFirstCharUppercase(name)}
+          <img src={front_default} />
+        </Typography>
+        <img style={{ width: "300px", height: "300px" }} src={fullImageUrl} />
+        <Typography variant="h3">Pokemon info</Typography>
+        <Typography>
+          {"Species: "}
+          <Link href={species.url}>{species.name}</Link>
+        </Typography>
+        <Typography>Height: {height}</Typography>
+        <Typography>Weight: {weight}</Typography>
+        <Typography variant="h6">Types:</Typography>
+        {types.map((typeInfo) => {
+          const { type } = typeInfo;
+          const { name } = type;
+          return <Typography key={name}> {`${name}`}</Typography>;
+        })}
+      </>
+    );
+  };
+
+  return <>{generatePokemonJSX()}</>;
 };
 
 export default Pokemon;
