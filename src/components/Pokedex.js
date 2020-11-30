e70e919639287b2df83727968a1f69a2b1245de9
Levente Csoke <leventec3@gmail.com>
2020-11-30 15:04:24 +0100

    part 2

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>hi</CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 const Pokedex = () => {
-  return <div>This is the pokedex page</div>;
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
+    </>
+  );
 };
 
 export default Pokedex;
